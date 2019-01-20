c78f44541aa9f5a6b88a31123ba6a6b8661a6054
mehrdad ghomi <mehrdadghomi@yahoo.com>
2019-01-20 15:20:40 -0800

    FOR ANDY commit
    3D arrow under construction
    2D indicator works fine

---
+++ Locator/Assets/Locator-Examples/Test Scene/Locator tools/FeedbackManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity;
+/*
+public class FeedbackManager : Singleton<FeedbackManager>
+{
+    
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    public void AddFeedback(GameObject GO)
+    {
+        HighlightManager.AddHighlight(GO);
+        
+    }
+    
+}
+*/
---
+++ Locator/Assets/myConstantRotation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class myConstantRotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Rotate(0, 1, 0 * Time.deltaTime); //rotates 50 degrees per second around z axis
+    }
+}
